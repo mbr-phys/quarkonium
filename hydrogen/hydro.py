615d89c52edaecbe4d91eac7ce195f34cc7b300e
mbr-phys <misconshark@protonmail.com>
2020-10-02 23:28:37 +0100

    update

--- hydrogen/hydro.py
+++ hydrogen/hydro.py
@@ -7,7 +7,7 @@ from h2_2 import wavefn, sqr, simpson, psi, counter, itera, statement
 
 u0 = [0, 1] #array of u and du/dr vals at 0
 
-r1 = np.linspace(0.001, 5200, 1000)
+r1 = np.linspace(1e-5, 6000, 1000)
 r2 = np.linspace(0.001, 6200, 1000)
 r3 = np.linspace(0.001, 9500, 1000)
 
@@ -57,13 +57,13 @@ ax = f1.add_subplot(111)
 ax.tick_params(axis='x', which='major', labelsize=25)
 ax.tick_params(axis='y', which='major', labelsize=25)
 plt.plot(r1, sol1, 'b',label="(n,l) = (1,0), $E_{nl} =$ %.1f eV" % En1)
-plt.plot(r2, sol20, 'r',label="(n,l) = (2,0), $E_{nl} =$ %.2f eV" % En20)
-plt.plot(r2, sol21, 'g',label="(n,l) = (2,1), $E_{nl} =$ %.2f eV" % En21)
-plt.plot(r3, sol30, 'purple',label="(n,l) = (3,0), $E_{nl} =$ %.2f eV" % En30)
-plt.plot(r3, sol31, 'cyan',label="(n,l) = (3,1), $E_{nl} =$ %.2f eV" % En31)
-plt.plot(r3, sol32, 'm',label="(n,l) = (3,2), $E_{nl} =$ %.2f eV" % En32)
+#plt.plot(r2, sol20, 'r',label="(n,l) = (2,0), $E_{nl} =$ %.2f eV" % En20)
+#plt.plot(r2, sol21, 'g',label="(n,l) = (2,1), $E_{nl} =$ %.2f eV" % En21)
+#plt.plot(r3, sol30, 'purple',label="(n,l) = (3,0), $E_{nl} =$ %.2f eV" % En30)
+#plt.plot(r3, sol31, 'cyan',label="(n,l) = (3,1), $E_{nl} =$ %.2f eV" % En31)
+#plt.plot(r3, sol32, 'm',label="(n,l) = (3,2), $E_{nl} =$ %.2f eV" % En32)
 ## probs get gridspec in here for separate plots
-plt.plot((r3[0],r3[-1]),(0,0),'grey')
+plt.plot((r1[0],r1[-1]),(0,0),'grey')
 
 plt.legend(loc=1, fontsize=35)
 plt.title("Solutions of the Hydrogen Electron Radial Wavefunction",fontsize=25)
